fc7e95400d67d20612ba5e13b3332b436611f93f
= <rafaelvieirapdm@gmail.com>
2025-06-13 11:53:18 -0300

    continuar em casa

--- JKalangoconsole/src/br/com/Jkalango/bo/IColor.java
+++ JKalangoconsole/src/br/com/Jkalango/bo/IColor.java
@@ -1,5 +1,6 @@
 package br.com.Jkalango.bo;
-
+import java.awt.Color;
 public interface IColor {
-public abstract void mudarfundo();
+public abstract void mudarfundo(Color  newColor);
+public abstract void mudartexto(Color newColor);
 }
--- JKalangoconsole/src/br/com/Jkalango/view/JMissaoobjeto.java
+++ JKalangoconsole/src/br/com/Jkalango/view/JMissaoobjeto.java
@@ -1,10 +1,25 @@
 package br.com.Jkalango.view;
 import br.com.Jkalango.bo.IColor;
 import javax.swing.JFrame;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.BorderFactory;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class JMissaoobjeto extends JFrame implements IColor{
 
 
-      
+
       @Override
       public void mudarfundo() {
             // TODO Auto-generated method stub
